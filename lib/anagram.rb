a76abb3f08ddd9ff971555504b4fca4a330e7fb9
Miriam Castillo <castillo.m.mc@gmail.com>
2018-08-22 04:29:03 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -25,9 +25,9 @@ class Anagram
       term.split("").sort == @word.split("").sort
     end
 
-    #created a class method called match, passed in an argument of 'array' (is literally an array) 
+    #created a class method called match, passed in an argument of 'array' (is literally an array)
     #iterated over the 'array' array, used the enumerable .find_all on |term|
-    
+    #
 
   end
 
